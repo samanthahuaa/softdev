160866bf4ad659b43b3279469113d9818a9bf9d9
samanthahuaa <shua30@stuy.edu>
2023-04-11 01:02:15 -0400

    my stomach hurts

--- 28_jsdom/dommy_consolery.js
+++ 28_jsdom/dommy_consolery.js
@@ -1,24 +1,4 @@
-/*
-   your PPTASK:
-
-   Test drive each bit of code in this file,
-    and insert comments galore, indicating anything
-     you discover,
-    	have questions about,
-    		or otherwise deem notable.
-
-    		Write with your future self or teammates in mind.
-
-    		If you find yourself falling out of flow mode, consult
-    		other teams
-    		MDN
-
-   A few comments have been pre-filled for you...
-
-   (delete this block comment once you are done)
-*/
-
-// Team guac no salsa :: Samantha Hua
+// Team guac no salsa :: Samantha Hua, Vansh Saboo
 // SoftDev pd0
 // K28 -- Getting more comfortable with the dev console and the DOM
 // 2023-04-05w
@@ -41,6 +21,7 @@ var f = function(x) {
 
 
 //instantiate an object
+// you can access their fields with o["{name of field}"] (like a dictionary)
 var o = { 'name' : 'Thluffy',
           age : 1024,
           items : [10, 20, 30, 40],
@@ -59,12 +40,38 @@ var addItem = function(text) {
 };
 
 
-var removeItem = function(n) {
-  var listitems = document.getElementsByTagName('li');
-  listitems[n].remove();
+var addFact = function() {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = "7! = " + fact(7);
+  list.appendChild(newitem);
+};
+
+var addFib = function() {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = "F_7 = " + fib(7);
+  list.appendChild(newitem);
+};
+
+var addGCD = function() {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = "GCD of 280 and 49 is " + gcd(280, 49);
+  list.appendChild(newitem);
 };
 
 
+// Removes element at n-th index
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove(); 
+};
+
+
+// Items 2 through 7 are always overlaid blue
+// Red has a priority over black, blue has a priority over red
+// This function makes all of the elements in the list red by default
 var red = function() {
   var items = document.getElementsByTagName("li");
   for(var i = 0; i < items.length; i++) {
@@ -72,7 +79,8 @@ var red = function() {
   }
 };
 
-
+// According to the list (0-indexed), even indexed elements are made red, and odd indexed elements are made blue
+// In the HTML depiction (1-indexed), the odds are made red and the evens are made blue
 var stripe = function() {
   var items = document.getElementsByTagName("li");
   for(var i = 0; i < items.length; i++) {
@@ -84,10 +92,28 @@ var stripe = function() {
   }
 };
 
-//insert your implementations here for...
-// FIB
-// FAC
-// GCD
+function fact(x) {
+    if (x < 2) {
+        return 1 // base case
+    }
+    return x*fact(x-1)
+}
+
+
+function fib(x) {
+    if (x < 2) {
+        return x // base case
+    }
+    return fib(x-1)+fib(x-2)
+}
+
+function gcd(a, b){
+  if(b == 0){
+    return a
+  }
+  return gcd(b, a % b)
+}
+
 
 // In addition to the style shown above,
 //  you are encouraged to test drive the "arrow function syntax" as shown below.
@@ -96,3 +122,8 @@ const myFxn = (param1, param2) => {
   // body
   return retVal;
 };
+
+
+addFact()
+addFib()
+addGCD()
